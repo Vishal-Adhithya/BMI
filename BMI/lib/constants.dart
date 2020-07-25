c23994cb527fd5181972e30737995aff051f10b5
Vishal-Adhithya <vishaladhithya19@gmail.com>
2020-07-25 19:54:35 +0530

    made a separete file to store const

---
+++ BMI/lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFF111328);
+const kBottomContainerColour = Color(0xFFeb1555);
+
+const KLableTextStyle = TextStyle(
+  fontSize: 18,
+  color: Color(0xFF8d8e98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
+
+const kMinNumber = 120.0;
+const kMaxNumber = 220.0;
